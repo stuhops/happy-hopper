3837dd5a95b665001f1e9910ace63d1004521fa8
Stuart Hopkins <stuarth@gurutechnologies.net>
2023-02-26 11:29:06 -0700

    Swapped back to the original collision detection but none of it works

--- src/app/services/collision/collision.service.ts
+++ src/app/services/collision/collision.service.ts
@@ -8,23 +8,29 @@ import { WHSize } from 'src/app/models/wh-size.model';
 })
 export class CollisionService {
   static lineCircleIntersect(line: Line, circle: Circle): boolean {
-    const pointVector: Coords = {
-      x: line.point2.x - line.point1.x,
-      y: line.point2.y - line.point1.y,
-    };
-    const pointCircleVector: Coords = {
-      x: circle.center.x - line.point1.x,
-      y: circle.center.y - line.point1.y,
-    };
-    const vectDistSqrd: number = pointVector.x * pointVector.x + pointVector.y * pointVector.y;
-    const dot: number = pointVector.x * pointCircleVector.x + pointVector.y * pointCircleVector.y;
-    const circleLineDistSqrd =
-      circle.center.x ** 2 +
-      circle.center.y ** 2 -
-      line.point1.x ** 2 -
-      line.point1.y ** 2 -
-      dot ** 2 / vectDistSqrd;
-    return Math.abs(circleLineDistSqrd) <= circle.radius ** 2;
+    const v1 = { x: line.point2.x - line.point1.x, y: line.point2.y - line.point1.y };
+    const v2 = { x: line.point1.x - circle.center.x, y: line.point1.y - circle.center.y };
+    const b = -2 * (v1.x * v2.x + v1.y * v2.y);
+    const c = 2 * (v1.x * v1.x + v1.y * v1.y);
+    const d = Math.sqrt(
+      b * b - 2 * c * (v2.x * v2.x + v2.y * v2.y - circle.radius * circle.radius),
+    );
+    if (isNaN(d)) {
+      // no intercept
+      return false;
+    }
+    // These represent the unit distance of point one and two on the line
+    const u1 = (b - d) / c;
+    const u2 = (b + d) / c;
+    if (u1 <= 1 && u1 >= 0) {
+      // If point on the line segment
+      return true;
+    }
+    if (u2 <= 1 && u2 >= 0) {
+      // If point on the line segment
+      return true;
+    }
+    return false;
   }
 
   static rectCircleIntersect(coords: Coords, size: WHSize, circle: Circle): boolean {
@@ -32,31 +38,17 @@ export class CollisionService {
     const topRight: Coords = { x: coords.x + size.width, y: coords.y };
     const bottomRight: Coords = { x: coords.x + size.width, y: coords.y + size.height };
     const bottomLeft: Coords = { x: coords.x, y: coords.y + size.height };
-    return (
-      this.lineCircleIntersect(
-        // Top
-        { point1: topLeft, point2: topRight },
-        circle,
-      ) ||
-      this.lineCircleIntersect(
-        // Right
-        { point1: topRight, point2: bottomRight },
-        circle,
-      ) ||
-      this.lineCircleIntersect(
-        // Bottom
-        { point1: bottomRight, point2: bottomLeft },
-        circle,
-      ) ||
-      this.lineCircleIntersect(
-        // Left
-        { point1: bottomLeft, point2: topLeft },
-        circle,
-      ) ||
-      (circle.center.x > coords.x &&
-        circle.center.x < coords.x + size.width &&
-        circle.center.y > coords.y &&
-        circle.center.y < coords.y + size.height)
-    );
+
+    const top = this.lineCircleIntersect({ point1: topLeft, point2: topRight }, circle);
+    const right = this.lineCircleIntersect({ point1: topRight, point2: bottomRight }, circle);
+    const bottom = this.lineCircleIntersect({ point1: bottomRight, point2: bottomLeft }, circle);
+    const left = this.lineCircleIntersect({ point1: bottomLeft, point2: topLeft }, circle);
+    const center =
+      circle.center.x > coords.x &&
+      circle.center.x < coords.x + size.width &&
+      circle.center.y > coords.y &&
+      circle.center.y < coords.y + size.height;
+
+    return top || right || bottom || left || center;
   }
 }
